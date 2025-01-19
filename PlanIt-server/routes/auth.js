09c0f1db86cb3fa658f4422faa095b0ac0226c0c
yashi-15 <sharmayashi270@gmail.com>
2025-01-19 15:22:33 +0530

    password hashing and salt done

--- PlanIt-server/routes/auth.js
+++ PlanIt-server/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
 
 //create a new user [ POST : /api/auth/createuser ]
 //No login required
@@ -20,11 +21,16 @@ router.post("/createuser", [body("name", "Name should be of atleast 3 characters
             // Function is returned here if the user already exists
             return res.status(400).json({ error: "User with this email already exists" });
         }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+
         // Otherwise a new user is created
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPassword,
         });
         res.json(user);  //response returned ater creating user
     } 
