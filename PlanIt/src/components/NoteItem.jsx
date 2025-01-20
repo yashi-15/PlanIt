39851c3f6f1ffe502b248c08b1ed1e9c831f9996
yashi-15 <sharmayashi270@gmail.com>
2025-01-20 22:47:28 +0530

    remove not required css

--- PlanIt/src/components/NoteItem.jsx
+++ PlanIt/src/components/NoteItem.jsx
@@ -14,8 +14,8 @@ const NoteItem = (props) => {
                 </div>
                 <div className="flex">
 
-                <i className="fa-regular fa-pen-to-square mx-2 p-2 hover:rounded-full hover:bg-pink hover:text-white"></i>
-                <i className="fa-regular fa-trash-can mx-2 p-2 hover:rounded-full hover:bg-pink hover:text-white"></i>
+                <i className="fa-regular fa-pen-to-square mx-2 p-2 cursor-pointer hover:rounded-full hover:bg-pink hover:text-white"></i>
+                <i className="fa-regular fa-trash-can mx-2 p-2 cursor-pointer hover:rounded-full hover:bg-pink hover:text-white"></i>
                 </div>
             </div>
         </div>
